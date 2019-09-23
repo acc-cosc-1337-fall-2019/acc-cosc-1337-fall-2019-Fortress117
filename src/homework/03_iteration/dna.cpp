d3f1684e520506ea605baf5a6c2aa1f96c3a4dfa
Mason Noel <mason.noel@g.austincc.edu>
2019-09-23 12:06:39 -0500

    HW DNA started

--- src/homework/03_iteration/dna.cpp
+++ src/homework/03_iteration/dna.cpp
@@ -6,6 +6,10 @@ Calculate GC content:
 Iterate string count Gs and Cs, divide count by string length.
 Return quotient.
 */
+double get_gc_content(const std::string DNA)
+{
+	return 0.0;
+}
 
 
 
@@ -14,7 +18,10 @@ Return quotient.
 Write code for function get_reverse_string that
 accepts a string parameter and returns a string reversed.
 */
-
+std::string get_reverse_string(std::string RVDNA)
+{
+	return std::string();
+}
 
 
 /*
@@ -27,4 +34,7 @@ b. iterate local string variable and
 c. return string
 
 */
-
+std::string get_dna_complement(std::string DNACOMP)
+{
+	return std::string();
+}
