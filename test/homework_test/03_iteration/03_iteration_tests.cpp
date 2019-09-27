dae54644a4b436f50d54a25f1710f60b1401903a
Mason Noel <mason.noel@g.austincc.edu>
2019-09-26 19:32:40 -0500

    HW 3 9/26/19.

--- test/homework_test/03_iteration/03_iteration_tests.cpp
+++ test/homework_test/03_iteration/03_iteration_tests.cpp
@@ -2,22 +2,23 @@
 #include "catch.hpp"
 #include "dna.h"
 
-TEST_CASE("Verify Test Configuration", "verification") {
+TEST_CASE("Verify Test Configuration", "verification") 
+{
 	REQUIRE(true == true);
 }
 TEST_CASE("Test function get_gc_content(string dna)") 
 {
-	REQUIRE(get_gc_content(AGCTATAG) == .375);
-	REQUIRE(get_gc_content(CGCTATAG) == .50)
+	REQUIRE(get_gc_content("AGCTATAG") == .375);
+	REQUIRE(get_gc_content("CGCTATAG") == .50);
 }
 TEST_CASE("Test function get_reverse_string(string dna)")
 {
-	REQUIRE(get_reverse_string(AGCTATAG) == GATATCGA);
-	REQUIRE(get_reverse_string(CGCTATAG) == GATATCGA);
+	REQUIRE(get_reverse_string("AGCTATAG") == "GATATCGA");
+	REQUIRE(get_reverse_string("CGCTATAG") == "GATATCGA");
 }
 TEST_CASE("Test get dna complement")
 {
-	REQUIRE(get_dna_complement(AAAACCCGGT) == ACCGGGTTTT);
-	REQUIRE(get_dna_complement(CCCGGAAAAT) == ATTTTCCGGG);
+	REQUIRE(get_dna_complement("AAAACCCGGT") == "ACCGGGTTTT");
+	REQUIRE(get_dna_complement("CCCGGAAAAT") == "ATTTTCCGGG");
 	
 }
