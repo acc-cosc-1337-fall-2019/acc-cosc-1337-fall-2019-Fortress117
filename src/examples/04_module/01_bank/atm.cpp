c1ff4f3cf79df49701932824f86728f20148d43f
Fortress117 <mason.noel@g.austincc.edu>
2019-10-07 18:59:35 -0500

    Class 10/7/19 added atm (more classes stuff)

--- src/examples/04_module/01_bank/atm.cpp
+++ src/examples/04_module/01_bank/atm.cpp
@@ -1,1 +1,29 @@
+#include "atm.h"
+#include <iostream>
+
 //atm.cpp
+
+using std::cout;
+
+
+ATM::ATM(BankAccount a) :account(a)
+{
+
+	
+}
+void ATM::deposit(int amount)
+{
+	account.deposit(amount);
+
+}
+
+void ATM::withdraw(int amount)
+{
+	account.withdraw(amount);
+}
+
+void ATM::display_balance() const
+{
+
+	cout << "Balance: " << account.get_balance();
+}
