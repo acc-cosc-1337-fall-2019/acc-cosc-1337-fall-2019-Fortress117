1522768974edf560a991ccdd1fe44bf6438e9ee9
Fortress117 <mason.noel@g.austincc.edu>
2019-10-14 18:15:40 -0500

    customer cpp and h class stuff

--- src/examples/04_module/01_bank/customer.cpp
+++ src/examples/04_module/01_bank/customer.cpp
@@ -1,1 +1,20 @@
+#include "customer.h"
+
 //customer.cpp
+
+void Customer::add_account(BankAccount act)
+{
+	total_balance = total_balance + act.get_balance();
+	accounts.push_back(act);
+}
+
+ostream &  operator << (ostream & out, const Customer & c)
+{
+	out << "Customer accounts: \n";
+	for (auto act : c.accounts)
+	{
+		out << act;
+	}
+
+	out << "\n Total Balance: " << c.total_balance;
+}
--- src/examples/04_module/01_bank/customer.h
+++ src/examples/04_module/01_bank/customer.h
@@ -1,1 +1,18 @@
 //customer.h
+#include<vector>
+#include"bank_account.h"
+#include<iostream>
+
+using std::vector;
+using std::ostream;
+
+class Customer
+{
+public:
+	void add_account(BankAccount act);
+	friend ostream& operator<< (ostream& out, const Customer& c);
+
+private:
+	vector<BankAccount> accounts;
+	int total_balance{ 0 };
+};
