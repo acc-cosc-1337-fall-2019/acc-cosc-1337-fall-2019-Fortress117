9dd04d8dc96df129cb28f783f113bf619dab4e45
Fortress117 <mason.noel@g.austincc.edu>
2019-10-28 18:23:52 -0500

    dynamic memory start

--- src/examples/04_module/01_bank/main.cpp
+++ src/examples/04_module/01_bank/main.cpp
@@ -6,18 +6,11 @@ using std::cout;
 
 int main()
 {
-	int num = 5;
-	int & num_ref = num; //get the address of num
-	std::cout << "Address: " << &num_ref << std::endl; //output address
-	std::cout << "Value at above address: " << num_ref << std::endl; //output value at address of num
+	int * ptr_num = new int(5); //acquire dynamic memory
+	std::cout << "Address of: " << ptr_num << std::endl;
+	std::cout << "Value pointed to: " << *ptr_num;
 
+	delete ptr_num; //YOU MUST REMEMBER TO DELETE ALLOCATED MEMORY
 
-	int * val_ = &num_ref; //point to the address of num
-	std::cout << "Address pointed to: " << val_ << std::endl; //get the address of num
-	std::cout << "Value pointed to: " << *val_ << std::endl; //get the value pointed to by val_
-	
-	*val_ = 15;
-	std::cout << "Address pointed to: " << val_ << std::endl; //get the address of num
-	std::cout << "Value pointed to: " << *val_ << std::endl; //get the value pointed to by val_
 	return 0;
 }
