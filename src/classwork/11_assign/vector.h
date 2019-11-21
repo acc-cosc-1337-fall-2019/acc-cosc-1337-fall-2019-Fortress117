8d2226cc5161cb68eecd78dfcff14b9e9377f066
Art Gonzalez <arturo.gonzalez@austincc.edu>
2019-11-20 22:55:18 -0600

    move assignment incorrect

--- src/classwork/11_assign/vector.h
+++ src/classwork/11_assign/vector.h
@@ -9,7 +9,7 @@ public:
 	Vector(const Vector& v); //copy constructor.
 	Vector& operator=(const Vector& v); //copy assignment
 	Vector(Vector&& v); // move constructor
-	Vector& operator=(const Vector&& v); //move assignment
+	Vector& operator=(Vector&& v); //move assignment
 	size_t Size()const { return size; }
 	int& operator[](int i) { return nums[i]; }
 	int& operator[](int i)const { return nums[i]; }
